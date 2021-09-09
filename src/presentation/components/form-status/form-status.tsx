77acaf8e5db924dffcd1cb680ded0cc235aca2ac
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-09-08 22:03:37 -0300

    feat: ensure Login present error and hide spinner on authentication failure

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -14,7 +14,11 @@ const FormStatus: React.FC = () => {
     <div data-testid='error-wrap' className={Styles.errorWrap}>
       {isLoading && <Spinner className={Styles.spinner} />}
 
-      {mainError && <span className={Styles.error}>{mainError}</span>}
+      {mainError && (
+        <span data-testid='main-error' className={Styles.error}>
+          {mainError}
+        </span>
+      )}
     </div>
   )
 }
