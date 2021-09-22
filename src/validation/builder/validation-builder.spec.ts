d6abb6cb33b968b24c5f0f02dd8843a830b9778c
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-09-21 22:48:45 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/builder/validation-builder.spec.ts
+++ src/validation/builder/validation-builder.spec.ts
@@ -5,26 +5,51 @@ import {
   RequiredFieldValidation
 } from '@/validation/validators'
 
+import faker from 'faker'
+
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').required().build()
+    const field = faker.database.column()
 
-    expect(validations).toEqual([
-      new RequiredFieldValidation('any_field')
-    ])
+    const validations = sut.field(field).required().build()
+
+    expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 
   test('Should return EmailValidation', () => {
-    const validations = sut.field('any_field').email().build()
+    const field = faker.database.column()
 
-    expect(validations).toEqual([new EmailValidation('any_field')])
+    const validations = sut.field(field).email().build()
+
+    expect(validations).toEqual([new EmailValidation(field)])
   })
 
   test('Should return MinLengthValidation', () => {
-    const validations = sut.field('any_field').min(5).build()
+    const field = faker.database.column()
+    const length = faker.datatype.number()
+
+    const validations = sut.field(field).min(length).build()
 
     expect(validations).toEqual([
-      new MinLengthValidation('any_field', 5)
+      new MinLengthValidation(field, length)
+    ])
+  })
+
+  test('Should return a list of validations', () => {
+    const field = faker.database.column()
+    const length = faker.datatype.number()
+
+    const validations = sut
+      .field(field)
+      .required()
+      .min(length)
+      .email()
+      .build()
+
+    expect(validations).toEqual([
+      new RequiredFieldValidation(field),
+      new MinLengthValidation(field, length),
+      new EmailValidation(field)
     ])
   })
 })
