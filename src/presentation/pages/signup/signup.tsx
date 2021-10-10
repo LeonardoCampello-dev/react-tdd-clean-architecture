a2e6822d5bbb09a87d7094eb4940c3138bf08696
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-10-10 14:48:29 -0300

    feat: ensure SignUp change name status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,64 +1,51 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Styles from './signup-styles.scss'
 
-import {
-  LoginHeader,
-  Input,
-  FormStatus,
-  Footer
-} from '@/presentation/components'
+import { LoginHeader, Input, FormStatus, Footer } from '@/presentation/components'
 
 import Context from '@/presentation/contexts/form/form-context'
+import { Validation } from '@/presentation/protocols/validation'
 
-const SignUp: React.FC = () => {
-  const [state] = useState({
+type Props = {
+  validation: Validation
+}
+
+const SignUp: React.FC<Props> = ({ validation }: Props) => {
+  const [state, setState] = useState({
     isLoading: false,
-    nameError: 'Campo obrigatório',
+    name: '',
+    nameError: '',
     emailError: 'Campo obrigatório',
     passwordError: 'Campo obrigatório',
     passwordConfirmationError: 'Campo obrigatório',
     mainError: ''
   })
 
+  useEffect(() => {
+    setState({
+      ...state,
+      nameError: validation.validate('name', state.name)
+    })
+  }, [state.name])
+
   return (
     <div className={Styles.signup}>
       <LoginHeader />
 
-      <Context.Provider value={{ state }}>
+      <Context.Provider value={{ state, setState }}>
         <form action='' className={Styles.form}>
           <h2>Criar Conta</h2>
 
-          <Input
-            type='text'
-            name='name'
-            placeholder='Digite seu nome'
-          />
+          <Input type='text' name='name' placeholder='Digite seu nome' />
 
-          <Input
-            type='email'
-            name='email'
-            placeholder='Digite seu e-mail'
-          />
+          <Input type='email' name='email' placeholder='Digite seu e-mail' />
 
-          <Input
-            type='password'
-            name='password'
-            placeholder='Digite sua senha'
-          />
+          <Input type='password' name='password' placeholder='Digite sua senha' />
 
-          <Input
-            type='password'
-            name='passwordConfirmation'
-            placeholder='Repita sua senha'
-          />
+          <Input type='password' name='passwordConfirmation' placeholder='Repita sua senha' />
 
-          <button
-            data-testid='submit'
-            disabled
-            type='submit'
-            className={Styles.submit}
-          >
+          <button data-testid='submit' disabled type='submit' className={Styles.submit}>
             Entrar
           </button>
 
