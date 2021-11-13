1d1826a427691fc6491d7ea82dd239af116a6ffd
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-11-13 18:33:42 -0300

    test: ensure Input has focus when label is clicked

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 import faker from 'faker'
 
-import {
-  fireEvent,
-  render,
-  RenderResult
-} from '@testing-library/react'
+import { fireEvent, render, RenderResult } from '@testing-library/react'
 import Input from './input'
 
 import Context from '@/presentation/contexts/form/form-context'
@@ -38,5 +34,17 @@ describe('Input Component', () => {
     fireEvent.focus(input)
 
     expect(input.readOnly).toBe(false)
+  })
+
+  test('Should focus input on label click', () => {
+    const field = faker.database.column()
+    const sut = makeSut(field)
+
+    const input = sut.getByTestId(field)
+    const label = sut.getByTestId(`${field}-label`)
+
+    fireEvent.click(label)
+
+    expect(document.activeElement).toBe(input)
   })
 })
