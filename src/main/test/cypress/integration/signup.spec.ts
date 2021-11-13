8fc54a441d0a5f4c55cc720aa7d10467cf575712
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-11-13 17:47:18 -0300

    test: ensure Signup present valid state if form is valid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -8,11 +8,11 @@ describe('SignUp', () => {
   })
 
   it('Should load with correct initial state', () => {
-    cy.getByTestId('email').should('have.attr', 'readOnly')
-    FormHelper.testInputStatus('email', 'Campo obrigatório')
-
     cy.getByTestId('name').should('have.attr', 'readOnly')
     FormHelper.testInputStatus('name', 'Campo obrigatório')
+
+    cy.getByTestId('email').should('have.attr', 'readOnly')
+    FormHelper.testInputStatus('email', 'Campo obrigatório')
 
     cy.getByTestId('password').should('have.attr', 'readOnly')
     FormHelper.testInputStatus('password', 'Campo obrigatório')
@@ -25,11 +25,11 @@ describe('SignUp', () => {
   })
 
   it('Should present error state if form is invalid', () => {
+    cy.getByTestId('name').focus().type(faker.random.alphaNumeric(3))
+    FormHelper.testInputStatus('name', 'Valor inválido')
+
     cy.getByTestId('email').focus().type(faker.random.alphaNumeric(3))
     FormHelper.testInputStatus('email', 'Valor inválido')
-
-    cy.getByTestId('name').focus().type(faker.random.word())
-    FormHelper.testInputStatus('name', 'Valor inválido')
 
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
     FormHelper.testInputStatus('password', 'Valor inválido')
@@ -38,6 +38,25 @@ describe('SignUp', () => {
     FormHelper.testInputStatus('passwordConfirmation', 'Valor inválido')
 
     cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present valid state if form is valid', () => {
+    cy.getByTestId('name').focus().type(faker.name.findName())
+    FormHelper.testInputStatus('name')
+
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    FormHelper.testInputStatus('email')
+
+    const password = faker.random.alphaNumeric(5)
+
+    cy.getByTestId('password').focus().type(password)
+    FormHelper.testInputStatus('password')
+
+    cy.getByTestId('passwordConfirmation').focus().type(password)
+    FormHelper.testInputStatus('passwordConfirmation')
+
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
