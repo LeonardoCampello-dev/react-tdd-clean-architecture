577381572e7765a86f951a45a264b738ce5a3b65
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-11-13 18:08:06 -0300

    test: SignUp present correct error if email is in use

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,6 +1,19 @@
 import * as FormHelper from '../support/form-helper'
+import * as Http from '../support/signup-mocks'
 
 import faker from 'faker'
+
+const simulateValidSubmit = (): void => {
+  cy.getByTestId('email').focus().type(faker.internet.email())
+  cy.getByTestId('name').focus().type(faker.name.findName())
+
+  const password = faker.random.alphaNumeric(7)
+
+  cy.getByTestId('password').focus().type(password)
+  cy.getByTestId('passwordConfirmation').focus().type(password)
+
+  cy.getByTestId('submit').click()
+}
 
 describe('SignUp', () => {
   beforeEach(() => {
@@ -58,5 +71,15 @@ describe('SignUp', () => {
 
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present EmailInUseError on 403', () => {
+    Http.mockEmailInUseError()
+
+    simulateValidSubmit()
+
+    FormHelper.testMainError('Esse e-mail já está em uso')
+
+    FormHelper.testUrl('/signup')
   })
 })
