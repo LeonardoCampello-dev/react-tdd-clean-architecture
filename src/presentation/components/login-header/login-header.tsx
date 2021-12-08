268fdb37368be056a06ae9f206cbd990191ede1a
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-12-07 22:32:36 -0300

    refactor: adapt layout to mobile

--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -6,7 +6,7 @@ import Logo from '@/presentation/components/logo/logo'
 
 const LoginHeader: React.FC = () => {
   return (
-    <header className={Styles.header}>
+    <header className={Styles.headerWrap}>
       <Logo />
 
       <h1>4Dev - Enquetes para programadores</h1>
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -27,6 +27,38 @@ const SurveyList: React.FC = () => {
 
             <footer>Ver Resultado</footer>
           </li>
+
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbDown} className={Styles.iconWrap} />
+
+              <time>
+                <span className={Styles.day}>26</span>
+                <span className={Styles.month}>08</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+
+              <p>Qual é o seu framework web favorito?</p>
+            </div>
+
+            <footer>Ver Resultado</footer>
+          </li>
+
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
+
+              <time>
+                <span className={Styles.day}>26</span>
+                <span className={Styles.month}>08</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+
+              <p>Qual é o seu framework web favorito?</p>
+            </div>
+
+            <footer>Ver Resultado</footer>
+          </li>
         </ul>
       </div>
 
