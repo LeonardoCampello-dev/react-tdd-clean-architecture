2e71e97190fc0bf25f5d24356297731d998db459
Leonardo Campello <leonardocampello.dev@gmail.com>
2022-01-17 09:23:33 -0300

    refactor: change cache storage to sync

--- src/data/usecases/update-current-account/local-update-current-account.ts
+++ src/data/usecases/update-current-account/local-update-current-account.ts
@@ -12,6 +12,6 @@ export class LocalUpdateCurrentAccount implements UpdateCurrentAccount {
       throw new UnexpectedError()
     }
 
-    await this.setStorage.set('account', JSON.stringify(account))
+    this.setStorage.set('account', JSON.stringify(account))
   }
 }
