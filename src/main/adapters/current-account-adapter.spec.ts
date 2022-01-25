4974d48b4dd64908d6ec92a70b1d12568ce75d59
Leonardo Campello <leonardocampello.dev@gmail.com>
2022-01-24 22:15:39 -0300

    test: ensure CurrentAccountAdapter throws if an invalid account is provided

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,4 +1,5 @@
 import { mockAccountModel } from '@/domain/test'
+import { UnexpectedError } from '@/domain/errors'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 
 import { setCurrentAccountAdapter } from './current-account-adapter'
@@ -14,5 +15,11 @@ describe('CurrentAccountAdapter', () => {
     setCurrentAccountAdapter(account)
 
     expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+
+  test('Should throw UnexpectedError', () => {
+    expect(() => {
+      setCurrentAccountAdapter(undefined)
+    }).toThrow(new UnexpectedError())
   })
 })
